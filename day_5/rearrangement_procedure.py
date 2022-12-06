7a3864435f5fff19edc0c26f3b481d18ce8df6e5
your name <you@example.com>
2022-12-06 10:18:26 -0500

    comment cleaning

--- day_5/rearrangement_procedure.py
+++ day_5/rearrangement_procedure.py
@@ -2,10 +2,10 @@ FIN = "crate_instructions.txt"
 
 def main():
     columns = dict()
-    num_columns_found = 0
 
     with open(FIN) as fin:
-        # parse columns
+        # part 1: create crate stacks as lists and use them as values for the
+        # column number keys in a dictionary
         line = fin.readline()
         while line != "\n":
             count = 1
@@ -17,66 +17,49 @@ def main():
                     if count == 2:
                         if '1'  not in columns.keys():
                             columns['1'] = []
-                            num_columns_found += 1
                         columns['1'] += [elem]
                     else:
-                        # to calculate which column the found value goes into
-                        # based off how many characters into a line we are is a
-                        # reversal of this:
-                        # column_num x 3 + inc_val = count
-                        # using the hardcoded value of '6' for the place of the
-                        # second value
-                        x = (count - 6) / 4
-                        y = (count - x) / 3
+                        # there is a gap of 4 characters between each crate
+                        # value consumed.  the correlation between column
+                        # number(col_num) and characters consumed(count) can be
+                        # described as:
+                        # count = col_num x 3 + inc_val
+                        # where inc_val starts at 0 and increases by 1 for
+                        # every new column
+                        # to calculate which col_num the found value goes into
+                        # based off the count, we need a formula which
+                        # converts 6 to a inc_val of 0:
+                        # 6-6 = 0     ez pz
+                        # and 10 to a inc_val of 1, and so on:
+                        # 10-6 = 4/4 = 1
+                        # 14-6 = 8/4 = 2
+                        inc_val = (count - 6) / 4
+                        # so now we have both count and inc_val, and order of
+                        # operations follows naturally to reverse our
+                        # correlation formula
+                        col_num  = (count - inc_val) / 3
                         # reminder: division makes a float
-                        z = str(int(y))
+                        z = str(int(col_num))
                         if z not in columns.keys():
                             columns[z] = []
-                            num_columns_found += 1
                         columns[z] += [elem]
                 count += 1
             line = fin.readline()
 
-        #print(f"num columns: {num_columns_found}")
-        #print(f"column 1: {columns['1']}")
-        #print(f"column 2: {columns['2']}")
-        #print(f"column 3: {columns['3']}")
-        #print(f"column 4: {columns['4']}")
-        #print(f"column 5: {columns['5']}")
-        #print(f"column 6: {columns['6']}")
-        #print(f"column 7: {columns['7']}")
-        #print(f"column 8: {columns['8']}")
-        #print(f"column 9: {columns['9']}")
-        #print(f"columns: {columns}")
-        #return
-
-        # "move quantity from colA to colB"
+        # part 2: move crates from one 'stack' to another according to the format:
+        # "move quantity from col_A to col_B"
         line = fin.readline().split()
-        #counter = 0
         while line:
-            #if counter == 10:
-                #pass
-                #return
-            #print(f"{line}")
-            x = int(line[1])
-            #print(f"{columns[line[3]]}")
-            #print(f"{columns[line[5]]}")
+            x = int(line[1]) # quantity
             while x:
-                #print(f"{x}")
-                #print(f"before pop col {line[3]}: {columns[line[3]]}")
-                y = columns[line[3]].pop(0)
-                #print(f"after pop col {line[3]} : {columns[line[3]]}")
-                #print(f"before add col {line[5]}: {columns[line[5]]}")
-                columns[line[5]].insert(0, y)
-                #print(f"after add col {line[5]} : {columns[line[5]]}")
+                y = columns[line[3]].pop(0)   # col_A
+                columns[line[5]].insert(0, y) # col_B
                 x -= 1
             line = fin.readline().split()
-            counter += 1
 
         answer = ""
-        for i in range(1, num_columns_found + 1):
+        for i in range(1, len(columns) + 1):
             last = columns[str(i)][0]
-            #print(f"last elem: {last}")
             answer += last
         print(f"ANSWER: {answer}")
 
