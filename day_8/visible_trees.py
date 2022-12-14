106c1e29491b1fbc01ef92511a49dd6c0925b1e5
your name <you@example.com>
2022-12-13 19:08:09 -0500

    FINALLY - bad T/F and </>

--- day_8/visible_trees.py
+++ day_8/visible_trees.py
@@ -19,57 +19,55 @@ def main():
     for row in tree_grid:
         # perimeter is visible // top/bottom rows
         if row_index == 0 or row_index == (NUM_ROWS - 1):
-            #print(f"ROW_INDEX: {row_index}")
             answer += len(row)
         else:
             column_index = 0
             for tree in row:
                 # perimeter is visible // first/last column
                 if column_index == 0 or column_index == (NUM_COLS - 1):
-                    #print(f"COL_INDEX: {column_index}")
                     answer += 1
                 else:
-                    #print(f"CUR_LOCATION: [{row_index}][{column_index}]")
-                    # directional flags to indicate that a tree is visible
-                    # false = not visible / true = visible
-                    up_flag = False
-                    dn_flag = False
-                    lt_flag = False
-                    rt_flag = False
-                    # check vis up
+                    # tree is visible if it is the same or taller than ANY tree
+                    # in the row or column
+                    # true = visible / false = invisible 
+                    up_flag = True 
+                    dn_flag = True 
+                    lt_flag = True 
+                    rt_flag = True 
                     cur_row = row_index - 1
                     cur_col = column_index
+                    # check vis up
                     while cur_row >= 0:
-                        if int(tree) >= int(tree_grid[cur_row][cur_col]):
-                            up_flag = True
+                        if int(tree) <= int(tree_grid[cur_row][cur_col]):
+                            up_flag = False
                             break
                         cur_row -= 1
                     # check vis down 
                     cur_row = row_index + 1
                     while cur_row < NUM_ROWS:
-                        if int(tree) >= int(tree_grid[cur_row][cur_col]):
-                            dn_flag = True
+                        if int(tree) <= int(tree_grid[cur_row][cur_col]):
+                            dn_flag = False
                             break
                         cur_row += 1
                     # check vis left
                     cur_row = row_index
                     cur_col = column_index - 1
                     while cur_col >= 0:
-                        if int(tree) >= int(tree_grid[cur_row][cur_col]):
-                            lt_flag = True
+                        if int(tree) <= int(tree_grid[cur_row][cur_col]):
+                            lt_flag = False
                             break
                         cur_col -= 1
                     # check vis right
                     cur_row = row_index
                     cur_col = column_index + 1
                     while cur_col < NUM_COLS:
-                        if int(tree) >= int(tree_grid[cur_row][cur_col]):
-                            rt_flag = True
+                        if int(tree) <= int(tree_grid[cur_row][cur_col]):
+                            rt_flag = False
                             break
                         cur_col += 1
                     # if any directional flag is still false, then that tree is
                     # visible from that direction
-                    if not up_flag or not dn_flag or not lt_flag or not rt_flag:
+                    if up_flag or dn_flag or lt_flag or rt_flag:
                         answer += 1
 
                 column_index += 1
@@ -79,10 +77,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# guess 197  - too low
-# guess 392  - no response 
-# guess 964  - no response
-# guess 1150 - no response
-# guess 2817 - too high
-# guess 9801 - too high
