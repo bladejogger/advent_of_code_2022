cb3a6cf630575fc64c861bd8e8682bd0f197ce30
your name <you@example.com>
2022-12-20 00:59:24 -0500

    reduction

--- day_9/tail_tracking.py
+++ day_9/tail_tracking.py
@@ -1,63 +1,39 @@
 FIN = "motion_paths.txt"
 
-SPOTS_VISITED = []
+DPAD = {'U': [0, 1], 'D': [0, -1], 'R': [1, 0], 'L': [-1, 0]}
+SPOTS_VISITED = [[0, 0]]
+HEAD = [0, 0]
+TAIL = [0, 0]
 
-def coord_check(coords):
-    if tuple(coords) not in SPOTS_VISITED:
-        SPOTS_VISITED.append(tuple(coords))
+def coord_check():
+    if TAIL not in SPOTS_VISITED:
+        SPOTS_VISITED.append(TAIL.copy())
+
+def move_snake(direction, distance):
+    global HEAD, TAIL
+    for i in range(0, int(distance)):
+        HEAD[0] += DPAD[direction][0]
+        HEAD[1] += DPAD[direction][1]
+
+        x_diff = abs(HEAD[0] - TAIL[0])
+        y_diff = abs(HEAD[1] - TAIL[1])
+        if x_diff > 1 or y_diff > 1:
+            if direction == 'U' or direction == 'D':
+                TAIL[0] = HEAD[0]
+                TAIL[1] += DPAD[direction][1]
+            else:
+                TAIL[0] += DPAD[direction][0]
+                TAIL[1] = HEAD[1]
+            coord_check()
 
 def main():
-    head = [0, 0]
-    tail = [0, 0]
-    SPOTS_VISITED.append(tuple(tail))
     with open(FIN) as fin:
         line = fin.readline().strip('\n').split()
         while line:
-            dist = int(line[1])
-            # upwards tracking 
-            if line[0] == 'U':
-                while dist > 0:
-                    dist -= 1
-                    head[1] += 1
-                    if abs(head[1] - tail[1]) > 1:
-                        tail[0] = head[0]
-                        tail[1] += 1
-                        coord_check(tail)
-            # downwards tracking
-            elif line[0] == 'D':
-                while dist > 0:
-                    dist -= 1
-                    head[1] -= 1
-                    if abs(head[1] - tail[1]) > 1:
-                        tail[0] = head[0]
-                        tail[1] -= 1
-                        coord_check(tail)
-            # leftwards tracking
-            elif line[0] == 'L':
-                while dist > 0:
-                    dist -= 1
-                    head[0] -= 1
-                    if abs(head[0] - tail[0]) > 1:
-                        tail[0] -= 1
-                        tail[1] = head[1]
-                        coord_check(tail)
-            # rightwards tracking
-            elif line[0] == 'R':
-                while dist > 0:
-                    dist -= 1
-                    head[0] += 1
-                    if abs(head[0] - tail[0]) > 1:
-                        tail[0] += 1
-                        tail[1] = head[1]
-                        coord_check(tail)
-            else:
-                print("ruh roh: found '{line[0]} {line[1]}'")
-                return
-
+            move_snake(line[0], line[1])
             line = fin.readline().strip('\n').split()
 
-    answer = len(SPOTS_VISITED)
-    print(f"answer: {answer}")
+    print(f"answer: {len(SPOTS_VISITED)}")
 
 if __name__ == "__main__":
     main()
